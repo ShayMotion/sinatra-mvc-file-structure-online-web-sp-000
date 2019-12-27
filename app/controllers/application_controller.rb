332ed25a99403babd10b699ed06afdce3df4b492
Shayan Mohsin <shaymotionfilms@gmail.com>
2019-12-27 03:37:06 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,23 @@
 class ApplicationController < Sinatra::Base
-class Dog 
-  
+class Dog
+
+  attr_accessor :name, :breed, :age
+
+    @@all = []
+
+  def initialize(name, breed, age)
+    @name = name
+    @breed = breed
+    @age = age
+    @@all << self
+  end
+
+  def self.all
+      @@all
+  end
+
+end
+
   configure do
   	set :views, "app/views"
   	set :public_dir, "public"
